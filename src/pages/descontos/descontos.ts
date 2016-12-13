804c65f6607ba927d48f7efd2bf6ef1fa4c37296
Moacir Jacomin <Moacir Jacomin>
2016-12-13 21:23:23 -0200

    v1 com listagem e exclusao cards

--- src/pages/descontos/descontos.ts
+++ src/pages/descontos/descontos.ts
@@ -11,11 +11,50 @@ export class DescontosPage {
   listaCards: Array<Object>;
   saida: string;
 
-  constructor(public navCtrl: NavController, public cards: Cards) {  this.listaCards = []; }
+  constructor(public navCtrl: NavController, public cards: Cards) {
+    this.listaCards = [];
+    //this.cards.iniciaCards();
+  }
 
   ionViewDidLoad() {
     console.log('Pag Desconto: iniciando');
   }
+
+  testePontoInicial(){
+     this.cards.adicionar({"id_desc":"10","categoria":"calcados","nome_resumo":"Card Teste 1","nome_completo":"Moa Cal\u00e7ado","data_hoje":"","validade":"2015-12-25","contato":"011 1406","local_cidade":"campinas-sp","local_detalhes":"Shopping Parque Don Pedro, campinas-s","observacoes":"","coordenadas":"-22.847656, -47.06423","img_card":" ","img_detalhes":""});
+     //                     di.id_desc   , di.categoria         , di.nome_resumo           , di.nome_completo                 , di.data_hoje , di.validade           , di.contato         , di.local_cidade            , di.local_detalhes                                      , di.observacoes , di.coordenadas                      , di.img_card , di.img_detalhes
+  }
+
+  testeAtualizaMaiorId(){
+    this.cards.atualizaMaiorId();
+    let num = this.cards.pegaMaiorId()
+    this.saida = num.toString();
+  }
+
+  testeRemove(idTR){
+    this.cards.remover(idTR);
+    this.listarCards();
+  }
+
+  listarCards(){
+    this.cards.retorna().then((data) => {
+       //console.log('listarPessoasDOIS saida: ',data);
+       console.log("listarCards tam: ", data.rows.length );
+       this.listaCards = [];
+
+       for(var i = 0; i < data.rows.length; i++) {
+           console.log(".."+data.rows.item(i).id_desc+": "+data.rows.item(i).nome_resumo);
+           this.listaCards.push({id: data.rows.item(i).id, id_desc: data.rows.item(i).id_desc, nome_resumo: data.rows.item(i).nome_resumo, categoria: data.rows.item(i).categoria});
+           //di.id_desc, di.categoria, di.nome_resumo, di.nome_completo, di.data_hoje, di.validade, di.contato, di.local_cidade, di.local_detalhes, di.observacoes, di.coordenadas, di.img_card, di.img_detalhes
+       }
+
+       console.log("listaCards: ", JSON.stringify(this.listaCards) );
+    }, (error) => {
+          console.log('Error', error.err);
+          //this.displayToast('Full of errors', 5000);
+    });
+  }
+
 
   pegaDescontos(){
     this.cards.buscaAtualizacoes('2')
@@ -25,6 +64,8 @@ export class DescontosPage {
         for (let i = 0; i < json.length; i++) {
             console.log('pegaDescontos: '+i+':', json[i].categoria, ' id=', json[i].id_desc, ' nome_resumo=', json[i].nome_resumo );
             this.listaCards.push(json[i]);
+
+            this.cards.adicionar({"id_desc":json[i].id_desc,"categoria":json[i].categoria,"nome_resumo":json[i].nome_resumo,"nome_completo":json[i].nome_completo,"data_hoje":json[i].data_hoje,"validade":json[i].validade,"contato":json[i].contato,"local_cidade":json[i].local_cidade,"local_detalhes":json[i].local_detalhes,"observacoes":json[i].observacoes,"coordenadas":json[i].coordenadas,"img_card":json[i].img_card,"img_detalhes":json[i].img_detalhes});
         }
 
         if(res.status == 200){
--- src/providers/cards.ts
+++ src/providers/cards.ts
@@ -1,24 +1,79 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { SQLite} from 'ionic-native';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
 
 @Injectable()
 export class Cards {
+  db = new SQLite();
+  maiorId: number;
 
   constructor(public http: Http) {
-    console.log('CARDS: iniciando o provider');
+     console.log('CARDS: iniciando o provider');
+     this.db.openDatabase({
+         name: "meu10conto.db",
+         location: "default"
+     }).then(() => {
+         this.db.executeSql("CREATE TABLE IF NOT EXISTS cards (id INTEGER PRIMARY KEY AUTOINCREMENT, id_desc INTEGER, categoria TEXT, nome_resumo TEXT, nome_completo TEXT, data_hoje TEXT, validade TEXT, contato TEXT, local_cidade TEXT, local_detalhes TEXT, observacoes TEXT, coordenadas TEXT, img_card TEXT, img_detalhes TEXT)", {}).then((data) => {
+                          console.log("CARS: tabela CARDS criada ", data);
+         }, (error) => {  console.error("CARS: erro ao criar tabela CARDS", error);        })
+     }, (error) => {      console.error("Unable to open db", error);                       });
   }
 
-  getCep( cep : string) : Promise <Response>{
-      return this.http.get('http://viacep.com.br/ws/'+ cep.trim() +'/json/').toPromise();
+  iniciaCards(){
+     this.db.openDatabase({
+         name: "meu10conto.db",
+         location: "default"
+     }).then(() => {
+         this.db.executeSql("CREATE TABLE IF NOT EXISTS cards (id INTEGER PRIMARY KEY AUTOINCREMENT, id_desc INTEGER, categoria TEXT, nome_resumo TEXT, nome_completo TEXT, data_hoje TEXT, validade TEXT, contato TEXT, local_cidade TEXT, local_detalhes TEXT, observacoes TEXT, coordenadas TEXT, img_card TEXT, img_detalhes TEXT)", {}).then((data) => {
+                          console.log("CARS: tabela CARDS criada ", data);
+         }, (error) => {  console.error("CARS: erro ao criar tabela CARDS", error);        })
+     }, (error) => {      console.error("Unable to open db", error);                       });
+  }
+
+  public atualizaMaiorId() {
+       this.db.executeSql("SELECT MAX(id_desc) AS ult FROM cards", []).then((data) => {
+           if(data.rows.length > 0) { this.maiorId = data.rows.item(0).ult;
+           }else{                     this.maiorId = 0;   }
+           console.log("CARDS atualizaMaiorId(): maiorId=" + this.maiorId);
+       }, (error) => {
+           console.log("CARDS atualizaMaiorId(): maiorId=" + this.maiorId);
+       });
+  }
+
+  pegaMaiorId(){
+     return this.maiorId;
+  }
+
+  public remover(idR) {
+    console.log("CARDS: tentei REMOVER: " + idR );
+     this.db.executeSql("DELETE FROM cards WHERE id = ? ", [idR]).then((data) => {
+                     console.log("CARDS: REMOVI: "  + idR);
+     }, (error) => { console.log("CARDS: ERROR: " + JSON.stringify(error.err));
+     });
+  }
+
+  public adicionar(di) {
+    console.log("CARDS: tentei INSERIR: " + JSON.stringify(di) );
+     this.db.executeSql("INSERT INTO cards (id_desc, categoria, nome_resumo, nome_completo, data_hoje, validade, contato, local_cidade, local_detalhes, observacoes, coordenadas, img_card, img_detalhes) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?,?)", [di.id_desc, di.categoria, di.nome_resumo, di.nome_completo, di.data_hoje, di.validade, di.contato, di.local_cidade, di.local_detalhes, di.observacoes, di.coordenadas, di.img_card, di.img_detalhes]).then((data) => {
+         console.log("CARDS: INSERIU: " + JSON.stringify(data));
+     }, (error) => {
+         console.log("CARDS: ERROR: " + JSON.stringify(error.err));
+     });
+  }
+
+  public retorna(){
+      return this.db.executeSql("SELECT * FROM cards", []);
   }
 
   buscaAtualizacoes(qtde:string){
       return this.http.get('http://www.moacir.net/meu10conto/Descontos.php?acao=dadosDescontosJSON&categoria=calcados&ult_reg=0&local=campinas-sp').toPromise();
       //return this.http.get('http://www.moacir.net/meu10conto/Descontos.php?acao=dadosDescontosJSON&categoria=calcados&ult_reg=0&local=campinas-sp').map(res => res.json()).toPromise();
+  }
 
-      //this.http.get('location/of/data').map(res => res.json()).subscribe(data => {    console.log(data); });
+  public limpar(){
+    return this.db.executeSql("DROP TABLE cards", []);
   }
 }
